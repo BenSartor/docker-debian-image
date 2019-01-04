0f85fd614fd7cfff64ded52762062e875cd2021f
Ben Sartor <ben-sartor.de>
2019-01-04 21:05:02 +0100

    create-debian-image.sh: deactivate script invocation on apt install

--- create-debian-image.sh
+++ create-debian-image.sh
@@ -4,8 +4,31 @@ set -eu -o pipefail
 declare -r DEBOOTSTAP_DIR=$1
 
 
+
+## debootstrap
 apt update
 apt dist-upgrade -y
 apt install -y debootstrap fakechroot fakeroot
 fakechroot fakeroot debootstrap --variant=minbase stretch "${DEBOOTSTAP_DIR}" http://deb.debian.org/debian
 
+
+
+## deactivate invoke of init.d scripts
+cat <<EOF > ${TMPDIR}/usr/sbin/policy-rc.d
+#!/bin/sh
+exit 101
+EOF
+
+chmod ugo+x ${TMPDIR}/usr/sbin/policy-rc.d
+
+
+
+## deactivate bootloader install with linux-image
+cat <<EOF > ${TMPDIR}/etc/kernel-img.conf
+do_symlinks = yes
+relative_links = yes
+do_bootloader = no
+do_bootfloppy = no
+do_initrd = no
+link_in_boot = no
+EOF
