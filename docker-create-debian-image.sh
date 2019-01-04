b90420a781d226c151d304110361f6230d3dba40
Ben Sartor <ben-sartor.de>
2019-01-04 22:52:03 +0100

    move to output dir

--- docker-create-debian-image.sh
+++ docker-create-debian-image.sh
@@ -10,12 +10,11 @@ declare -r BASE_DIR_DOCKER="/debian-docker"
 declare -r DOCKER_SCRIPT="${BASE_DIR_DOCKER}/create-debian-image.sh"
 declare -r DEBOOTSTAP_DIR_NAME="debootstrap"
 declare -r DEBOOTSTAP_DIR="${BASE_DIR}/${DEBOOTSTAP_DIR_NAME}"
-declare -r DEBOOTSTAP_DIR_DOCKER="${BASE_DIR_DOCKER}/${DEBOOTSTAP_DIR_NAME}"
 
 mkdir "${DEBOOTSTAP_DIR}"
 
 echo "debootstrab in a docker container"
-docker run --privileged --cap-add=SYS_ADMIN --cap-add MKNOD --security-opt apparmor:unconfined --rm -v "${BASE_DIR}":"${BASE_DIR_DOCKER}" debian:stretch-slim "${DOCKER_SCRIPT}" "${DEBOOTSTAP_DIR_DOCKER}"
+docker run --privileged --cap-add=SYS_ADMIN --cap-add MKNOD --security-opt apparmor:unconfined --rm -v "${BASE_DIR}":"${BASE_DIR_DOCKER}" debian:stretch-slim "${DOCKER_SCRIPT}" "${BASE_DIR_DOCKER}" "${DEBOOTSTAP_DIR_NAME}"
 
 echo "create docker image"
 tar -C "${DEBOOTSTAP_DIR}" -c . | docker import - debootstrap-stretch
