2409337392ed173362ea58305478d7c801eadd7b
Ben Sartor <ben-sartor.de>
2019-01-05 05:09:36 +0100

    slim image even more: as stretch-slim

--- create-debian-tar.sh
+++ create-debian-tar.sh
@@ -69,10 +69,24 @@ chroot "${DEBOOTSTAP_DIR}" bash -c "LANG=C DEBIAN_FRONTEND=noninteractive apt-ge
 echo "** reduce image size"
 cat <<EOF > "${DEBOOTSTAP_DIR}/etc/dpkg/dpkg.cfg.d/docker"
 path-exclude /usr/share/doc/*
+path-exclude /usr/share/doc/kde/HTML/*/*
+path-exclude /usr/share/gnome/help/*/*
+path-exclude /usr/share/groff/*
 path-exclude /usr/share/info/*
+path-exclude /usr/share/linda/*
 path-exclude /usr/share/lintian/overrides/*
 path-exclude /usr/share/locale/*
 path-exclude /usr/share/man/*
+path-exclude /usr/share/omf/*/*-*.emf
+path-include /usr/share/doc/*/copyright
+path-include /usr/share/doc/kde/HTML/C/*
+path-include /usr/share/gnome/help/*/C/*
+path-include /usr/share/locale/all_languages'
+path-include /usr/share/locale/currency/*'
+path-include /usr/share/locale/l10n/*
+path-include /usr/share/locale/languages'
+path-include /usr/share/locale/locale.alias
+path-include /usr/share/omf/*/*-C.emf
 EOF
 
 chroot "${DEBOOTSTAP_DIR}" bash -c "LANG=C DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --assume-yes --reinstall \$(dpkg --get-selections | grep -v deinstall | cut -f1 | sed \"s/:amd64\$//\")"
